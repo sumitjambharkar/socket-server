f2607482791dd794fc78df7c43e34aa642431763
sumit jambharkar <sumit@onehealthassist.com>
2024-09-05 15:06:50 +0530

    commit

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 // Attach Socket.IO to the server
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173","https://onehealthassist.com"],
+    origin: ["http://localhost:5173", "https://onehealthassist.com"],
     methods: ["GET", "POST"],
   }
 });
@@ -25,38 +25,50 @@ app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-const emailToSocketMapping = new Map()
-const socketIdToEmailMapping = new Map()
+const emailToSocketMapping = new Map();
+const socketIdToEmailMapping = new Map();
 
 // Setup Socket.IO connection event
-io.on('connection', (socket) => {   
+io.on('connection', (socket) => {
   console.log('A user connected');
-    socket.on("join-room", (data) => {
-      const {email,userId}  = data;
-      console.log(email + userId);
-      emailToSocketMapping.set(email, socket.id);
-      socketIdToEmailMapping.set(socket.id, email);
-      
-      io.to(userId).emit("userJoinedRoom",{email,id:socket.id})
-      socket.join(userId)
-      io.to(socket.id).emit("join-room",data)
-      
-    })
 
-    socket.on("userCalled",({to,offer})=>{
-      io.to(to).emit("incomingsCall",{from:socket.id,offer})
-    })
-    socket.on("callAccepted",({to,ans})=>{
-        io.to(to).emit("callAccepted",{from:socket.id,ans})
-    })
+  socket.on("join-room", (data) => {
+    const { email, userId } = data;
+    console.log(email + userId);
+    emailToSocketMapping.set(email, socket.id);
+    socketIdToEmailMapping.set(socket.id, email);
 
-    socket.on("peer-negotiation-need",({to,offer})=>{
-        io.to(to).emit("peer-negotiation-need",{from:socket.id,offer})
-    })
+    io.to(userId).emit("userJoinedRoom", { email, id: socket.id });
+    socket.join(userId);
+    io.to(socket.id).emit("join-room", data);
+  });
 
-    socket.on("negotiationneeded-done",({to,ans})=>{
-        io.to(to).emit("negotiationneeded-final",{from:socket.id,ans})
-    })
+  socket.on("userCalled", ({ to, offer }) => {
+    io.to(to).emit("incomingsCall", { from: socket.id, offer });
+  });
+
+  socket.on("callAccepted", ({ to, ans }) => {
+    io.to(to).emit("callAccepted", { from: socket.id, ans });
+  });
+
+  socket.on("peer-negotiation-need", ({ to, offer }) => {
+    io.to(to).emit("peer-negotiation-need", { from: socket.id, offer });
+  });
+
+  socket.on("negotiationneeded-done", ({ to, ans }) => {
+    io.to(to).emit("negotiationneeded-final", { from: socket.id, ans });
+  });
+
+  // Handle Mute/Unmute Event
+  socket.on("muteAudio", ({ to, muted }) => {
+    io.to(to).emit("remoteMute", { muted });
+  });
+
+  // Handle Call End Event
+  socket.on("endCall", ({ to }) => {
+    io.to(to).emit("callEnded");
+  });
+
   // Handle disconnection
   socket.on('disconnect', () => {
     const email = socketIdToEmailMapping.get(socket.id);
@@ -64,7 +76,6 @@ io.on('connection', (socket) => {
     socketIdToEmailMapping.delete(socket.id);
     console.log('A user disconnected');
   });
-  
 });
 
 // Listen on a port
